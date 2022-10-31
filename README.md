3fc489bdaf8d52b8918b4c886b5c48f6a4f9e8d7
Parth <parthsharma8715@outlook.com>
2022-10-30 21:07:10 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# COMP229-F2019-MidTerm Test
+
+## Welcome to the MidTerm Project - the Favourite Book List App
+
+please use **`npm install`** to install project dependencies
+"# PARTH_SHARMA_MIDTERM_COMP229" 
+"# PARTH_SHARMA_MIDTERM_COMP229" 
---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+ // "URI":"mongodb+srv://gayathri:gaya1234@cluster0.rxy9ytb.mongodb.net/book229?retryWrites=true&w=majority"
+"URI": "mongodb+srv://parth:parth12@cluster0.krjihkp.mongodb.net/book229?retryWrites=true&w=majority"
+//parth12
+};
